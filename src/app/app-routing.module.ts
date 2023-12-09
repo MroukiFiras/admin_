45a8347d70edb7af59f2a66c32c15dc998763d59
MroukiFiras <114618223+MroukiFiras@users.noreply.github.com>
2023-12-09 18:34:18 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,24 +6,90 @@ import { AuthAdminLayoutComponent } from './layout/auth-admin-layout/auth-admin-
 import { GuardadminGuard } from './views/guards/guardadmin.guard';
 
 const routes: Routes = [
-  { path: '', component: ClientLayoutComponent,children: [
-    {path: '', loadChildren: () => import('./views/client/home/home-routing.module').then((m) => m.HomeRoutingModule)},
-    {path: 'login',loadChildren: () =>import('./views/client/loginclient/loginclient.module').then((m) => m.LoginclientModule)},
-    {path: 'home', loadChildren: () => import('./views/client/home/home-routing.module').then((m) => m.HomeRoutingModule)},
-    {path: 'productDetail' , loadChildren: () => import('./views/client/product-detail/product-detail-routing.module').then((m) => m.ProductDetailRoutingModule )}
+  {
+    path: '',
+    component: ClientLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./views/client/home/home-routing.module').then(
+            (m) => m.HomeRoutingModule
+          ),
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./views/client/loginclient/loginclient.module').then(
+            (m) => m.LoginclientModule
+          ),
+      },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./views/client/home/home-routing.module').then(
+            (m) => m.HomeRoutingModule
+          ),
+      },
+      {
+        path: 'productDetail',
+        loadChildren: () =>
+          import(
+            './views/client/product-detail/product-detail-routing.module'
+          ).then((m) => m.ProductDetailRoutingModule),
+      },
     ],
   },
-  {path: 'admin',component: AdminLayoutComponent,canActivate:[GuardadminGuard],children: [
-      {path: '',loadChildren: () =>import('./views/admin/dashboard/dashboard.module').then((m) => m.DashboardModule)},
-      {path: 'dashboard',loadChildren: () =>import('./views/admin/dashboard/dashboard.module').then((m) => m.DashboardModule)},
-      {path: 'addProduct',loadChildren: () =>import('./views/admin/addprod/addprod/addprod.module').then((m) => m.AddprodModule)},
-      {path: 'products',loadChildren: () =>import('./views/admin/products/products/products.module').then((m) => m.ProductsModule)},
-      {path: 'orders',loadChildren: () =>import('./views/admin/ordersadmin/orders/orders.module').then((m) => m.OrdersModule)},
-      {path: 'customers', loadChildren: () =>import('./views/admin/customers/customers/customers.module').then((m) => m.CustomersModule)},
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    canActivate: [GuardadminGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./views/admin/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./views/admin/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+      {
+        path: 'addProduct',
+        loadChildren: () =>
+          import('./views/admin/addprod/addprod/addprod.module').then(
+            (m) => m.AddprodModule
+          ),
+      },
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./views/admin/products/products/products.module').then(
+            (m) => m.ProductsModule
+          ),
+      },
+      {
+        path: 'orders',
+        loadChildren: () =>
+          import('./views/admin/ordersadmin/orders/orders.module').then(
+            (m) => m.OrdersModule
+          ),
+      },
+      {
+        path: 'customers',
+        loadChildren: () =>
+          import('./views/admin/customers/customers/customers.module').then(
+            (m) => m.CustomersModule
+          ),
+      },
     ],
   },
-  {path: 'admin/login' , component:AuthAdminLayoutComponent}
-  
+  { path: 'admin/login', component: AuthAdminLayoutComponent },
 ];
 
 @NgModule({
